bd37a257fa64acce3b9f8b5baba58020d50da794
Markus Thulin <macky_b@hotmail.com>
2023-03-20 08:10:06 +0100

    Introduced partials for handlebars files

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
+const hbs = require("hbs");
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
 //Middleware
 app.use(cors());
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
-app.use('/', require('./routes/index'));
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 4567;
 
